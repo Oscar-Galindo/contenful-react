6488b57f232eb1cb9837dd3e3dd653e57a17144f
Oscar Galindo <oscargalindo@oscars-mbp.mynetworksettings.com>
2023-03-14 15:44:28 -0400

    updated sass, and added env

--- src/client.js
+++ src/client.js
@@ -1,10 +1,6 @@
 import { createClient } from 'contentful';
 
-const spaceId = 'ge20ega7j1zb';
-const accessToken = 'pl0nUNcIZBHMgtGopcWNm7KoP7eFWx0xvND_BFtyLaw';
-
-
 export const client = createClient({
-  space: spaceId ,
-  accessToken: accessToken
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
 });
